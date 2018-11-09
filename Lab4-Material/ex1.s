403b4da2cac0b7c7f3a43d23f41f393b94f40038
luk3Sky <ganindudananja@gmail.com>
2018-11-09 09:13:21 +0530

    excercise 1 started

---
+++ Lab4-Material/ex1.s
@@ -0,0 +1,43 @@
+@ ARM Assembly - exercise 1
+
+
+	.text 	@ instruction memory
+
+	
+@ Write YOUR CODE HERE	
+
+@ ---------------------	
+
+
+@ ---------------------	
+
+	.global main
+main:
+	@ stack handling, will discuss later
+	@ push (store) lr to the stack
+	sub sp, sp, #12
+	str lr, [sp, #0]
+
+	@ calling the scanf function
+	ldr r0, =format_scanf 		@the scanf format
+	mov r1, sp			@the arg2 load
+	bl scanf
+	ldr r0
+	
+
+	@ load aguments and print
+	ldr r0, =format1
+	mov r1, r4
+	mov r2, r5
+	mov r3, r6
+	bl printf
+
+	@ stack handling (pop lr from the stack) and return
+	ldr lr, [sp, #0]
+	add sp, sp, #4
+	mov pc, lr
+
+	.data	@ data memory
+format_scanf: .asciz "%d"
+format1: .asciz "Enter the number of strings :\n"
+
